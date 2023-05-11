c254b869f76bc6382e23910fb4a9b44c856168fe
nandz080 <nandz080@gmail.com>
2023-05-11 23:01:12 +0000

    [task 1]- bug fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
 
-    # Define variables
-    a = 10
-    b = 5
+	from calculator_1 import add, sub, mul, div
 
-    # Call functions and print the results
-    print('{} + {} = {}'.format(a, b, add(a, b)))
-    print('{} - {} = {}'.format(a, b, sub(a, b)))
-    print('{} * {} = {}'.format(a, b, mul(a, b)))
-    print('{} / {} = {}'.format(a, b, div(a, b)))
+	# Define variables
+	a = 10
+	b = 5
+	
+	# Perform calculations
+	add_result = add(a, b)
+	sub_result = sub(a, b)
+	mul_result = mul(a, b)
+	div_result = div(a, b)
+
+	# Print the results
+	print("{} + {} = {}".format(a, b, add_result))
+	print("{} - {} = {}".format(a, b, sub_result))
+	print("{} * {} = {}".format(a, b, mul_result))
+	print("{} / {} = {}".format(a, b, div_result))
