55a8cccd617dcb1d14600f05fa3c6a4b483d8411
nandz080 <nandz080@gmail.com>
2023-06-20 11:32:54 +0000

    [task 1]Write the first class Base with private class attribute

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" Module that contains class Base """
+import json
+import csv
+import os.path
+
+
+class Base:
+    """ Class Base """
+    __nb_objects = 0
+
+    def __init__(self, id=None):
+        """ Initializes instances """
+        if id is not None:
+            self.id = id
+        else:
+            Base.__nb_objects += 1
+            self.id = Base.__nb_objects
