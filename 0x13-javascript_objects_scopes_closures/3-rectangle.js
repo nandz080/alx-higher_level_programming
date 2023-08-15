05aeb96b4c0dfb60b357f0d6e6397023bbc9e7c0
nandz080 <nandz080@gail.com>
2023-08-15 14:27:22 -0700

    [task 3] fixed print function

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,12 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
   constructor(w, h) {
-    if (w > 0 && h > 0) {
-      [this.width, this.height] = [w, h];
-    }
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  print() {
-    if (this.width && this.height) {
-      for (let i = 0; i < this.height; i++) {
-        console.log('X'.repeat(this.width));
-      }
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
     }
   }
 };
